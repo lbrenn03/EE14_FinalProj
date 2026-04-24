27a758b25f7a7953344a75dfca8b4a2a6471b212
Leem958 <84033321+Leem958@users.noreply.github.com>
2026-04-23 21:33:08 -0400

    Refactor HostSerial.py for dynamic firmware handling
    Updated serial communication to handle firmware file size dynamically and adjusted timeout.

--- python/HostSerial.py
+++ python/HostSerial.py
@@ -1,20 +1,36 @@
+
 import serial
 import time
+import os
 
-# ser = serial.Serial(
-#     port = 'COM3',
-#     baudrate = 115200,
-#     timeout = 1
-# )
+ser = serial.Serial(
+    port = 'COM3',
+    baudrate = 115200,
+    timeout = 30
+)
+time.sleep(2)
 
-with open('NewCode.bin', 'rb') as file:
+size = os.path.getsize('firmware.bin')
+print(f"File size: {size} bytes")
+
+ser.read(size=4)
+ser.write(size.to_bytes(4, byteorder='little'))
+test = ser.read(size=6)
+print(test.decode("utf-8"))
+
+with open('firmware.bin', 'rb') as file:
         while True:
-            chunk = file.read(16)
+            chunk = file.read(size)
+            print(chunk.hex(' '))
             if not chunk:
                 break
+            ser.write(chunk)
 
-        print(chunk.hex())
 
+response = ser.read(size=size)
+print("Board said:", response.hex(' '))
+
+ser.close()
 
 
 
